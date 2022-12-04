ea2f1c6448b4cf4a5f31376dbcbee375acae271c
Utsav Kansal <kansalutsav29@gmail.com>
2022-12-04 23:55:53 +0530

    feature

--- src/modules/auth/firebase.js
+++ src/modules/auth/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBnUrMc85mRB5jVZXE78-UecjFs6tfyfyI",
+  authDomain: "onestopauth-4ece8.firebaseapp.com",
+  projectId: "onestopauth-4ece8",
+  storageBucket: "onestopauth-4ece8.appspot.com",
+  messagingSenderId: "399912268257",
+  appId: "1:399912268257:web:67b12b9065d50ca4c2a2a0",
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
