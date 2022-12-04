f66c46ec6daffcd27731936ee8aa72d20ff4ace9
Apurva Bhardwaj <e19cse203@bennett.edu.in>
2022-12-04 14:30:42 +0530

    Images added to assets directory

--- src/pages/contests/Contests.jsx
+++ src/pages/contests/Contests.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import styles from "./Contests.module.css";
-import upcomingImg from "./img/1.png";
-import finishedImg from "./img/2.png";
-import currentImg from "./img/3.png";
+import upcomingImg from "../../assets/images/upcoming_card.png";
+import ongoingImg from "../../assets/images/current_card.png";
+import finishedImg from "../../assets/images/past_card.png";
+
 import { Card, Modal, Button, Table } from "antd";
 import { data_future, data_past, data_present } from "./data";
 
@@ -35,9 +36,9 @@ const Contests = () => {
             className={styles["card-antd"]}
             hoverable
             style={{ width: 240 }}
-            cover={<img alt="example" src={upcomingImg} />}
+            cover={<img alt="example" src={ongoingImg} />}
           >
-            <Meta title="Current Contests" description="Codechef" />
+            <Meta title="Ongoing Contests" description="Codechef" />
           </Card>
         </div>
         <div
@@ -48,9 +49,9 @@ const Contests = () => {
             className={styles["card-antd"]}
             hoverable
             style={{ width: 240 }}
-            cover={<img alt="example" src={currentImg} />}
+            cover={<img alt="example" src={upcomingImg} />}
           >
-            <Meta title="Past Contests" description="Codechef" />
+            <Meta title="Upcoing Contests" description="Codechef" />
           </Card>
         </div>
         <div
@@ -63,12 +64,12 @@ const Contests = () => {
             style={{ width: 240 }}
             cover={<img alt="example" src={finishedImg} />}
           >
-            <Meta title="Upcoming Contests" description="Codechef" />
+            <Meta title="Past Contests" description="Codechef" />
           </Card>
         </div>
       </div>
       <Modal
-        title="Current Contests"
+        title="Ongoing Contests"
         style={{
           top: 20,
         }}
@@ -81,37 +82,7 @@ const Contests = () => {
           </Button>,
         ]}
       >
-        <Table dataSource={data_present}>
-          <Column
-            title="Contest Name"
-            dataIndex="name"
-            key="name"
-            render={(_, text) => (
-              <a href={"https://www.codechef.com/" + text.code}>{text.name}</a>
-            )}
-          />
-          <Column title="Start Date" dataIndex="s_date" key="s_date" />
-          <Column title="Start Time" dataIndex="s_time" key="s_time" />
-          <Column title="End Date" dataIndex="e_date" key="e_date" />
-          <Column title="End Time" dataIndex="e_time" key="e_time" />
-        </Table>
-        ;
-      </Modal>
-      <Modal
-        title="Past Contests"
-        style={{
-          top: 20,
-        }}
-        open={modal2Open}
-        onCancel={() => setModal2Open(false)}
-        width={1000}
-        footer={[
-          <Button className="button-antd" key="back" onClick={handle2Cancel}>
-            Return
-          </Button>,
-        ]}
-      >
-        <Table dataSource={data_past} pagination={{ pageSize: 5 }}>
+        <Table dataSource={data_present} pagination={{ pageSize: 5 }}>
           <Column
             title="Contest Name"
             dataIndex="name"
@@ -132,6 +103,36 @@ const Contests = () => {
         style={{
           top: 20,
         }}
+        open={modal2Open}
+        onCancel={() => setModal2Open(false)}
+        width={1000}
+        footer={[
+          <Button className="button-antd" key="back" onClick={handle2Cancel}>
+            Return
+          </Button>,
+        ]}
+      >
+        <Table dataSource={data_future} pagination={{ pageSize: 5 }}>
+          <Column
+            title="Contest Name"
+            dataIndex="name"
+            key="name"
+            render={(_, text) => (
+              <a href={"https://www.codechef.com/" + text.code}>{text.name}</a>
+            )}
+          />
+          <Column title="Start Date" dataIndex="s_date" key="s_date" />
+          <Column title="Start Time" dataIndex="s_time" key="s_time" />
+          <Column title="End Date" dataIndex="e_date" key="e_date" />
+          <Column title="End Time" dataIndex="e_time" key="e_time" />
+        </Table>
+        ;
+      </Modal>
+      <Modal
+        title="Past Contests"
+        style={{
+          top: 20,
+        }}
         open={modal3Open}
         onCancel={() => setModal3Open(false)}
         width={1000}
@@ -141,7 +142,7 @@ const Contests = () => {
           </Button>,
         ]}
       >
-        <Table dataSource={data_future}>
+        <Table dataSource={data_past} pagination={{ pageSize: 5 }}>
           <Column
             title="Contest Name"
             dataIndex="name"
